72f4080ae3250cc4b249ad0e04ef102501138157
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-05-18 16:28:35 +0200

    snack 2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 //SNACK 1
 //Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 //Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
@@ -27,7 +28,41 @@ $partite = [
 ];
 
 //var_dump($partite);
-   
+
+//SNACK 2
+//Passare come parametri GET name, mail e age 
+//verificare (cercando i metodi che non conosciamo nella documentazione) 
+//che name sia più lungo di 3 caratteri, 
+//che mail contenga un punto e una chiocciola e 
+//che age sia un numero. 
+//Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+$name = $_GET['name'];
+$mail = $_GET['mail'];
+$age = $_GET['age'];
+$age_int = intval($age);
+
+
+//var_dump($name);
+//var_dump($mail);
+//var_dump($age);
+//var_dump(intval($age));
+
+$risultato = 'Accesso riuscito';
+
+if(strlen($name) < 3){
+    echo 'Accesso negato';
+
+}elseif(strpos($mail, '@') === false || strpos($mail, '.') === false){
+    echo 'Accesso negato';
+
+}elseif($age != $age_int || $age_int === 0){
+    echo 'Accesso negato';
+
+}else{
+    echo $risultato;
+};
+
 ?>
 
 <!DOCTYPE html>
